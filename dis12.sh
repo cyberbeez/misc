d2d5440c66ff0c960e4a853c6971eaeaba7e45ff
Cyber Beez <150989206+cyberbeez@users.noreply.github.com>
2023-12-03 09:28:11 +1300

    Add files via upload

--- dis12.sh
+++ dis12.sh
@@ -2,33 +2,43 @@
 echo"Script to Run Network Management Commands"
 echo "--"
 echo "Check Ip Address:"
+echo "--"
 ip addr
 echo "--" 
 echo "Check Mac Addresses:"
-ip -o link show |cut -d ‘ ‘ -f 2,20
+echo "--"
+ip link show ens18
 echo "--" 
 echo "Check ARP table:"
-ARP -t ib -a
+echo "--"
+arp -n
 echo "--" 
 echo "Check IP Routing Tables:"
+echo "--"
 netstat -r
 echo "--" 
 echo "Check Open Ports:"
+echo "--"
 sudo ss -tunl
 echo "--" 
 echo "Capture Traffic:"
+echo "--"
 echo "Output to ./netcapt.pcap file."
 echo "I had to watch the video on this one, my method only works on debian..."
 timeout 10s sudo tcpdump -eth18 -w ./netcapt.pcap
+echo "using the 'tcpdump -eth18 -w ./netcapt.pcap' command..."
 echo "--" 
 echo "Check CPU and Memory Usage:"
+echo "--"
 vmstat
 free
 echo "--" 
 echo "Process Status:"
-timeout 2s top
+echo "--"
+ps -au
 echo "--" 
 echo "Stop a service and start a service:"
+echo "--"
 systemctl status sshd
 systemctl stop sshd
 systemctl status sshd
@@ -36,4 +46,8 @@ systemctl start sshd
 systemctl status sshd
 echo "--" 
 echo "Retrieve Kernel Version:"
+echo "--"
 hostnamectl
+echo "--"
+echo "--"
+echo "Done!"
